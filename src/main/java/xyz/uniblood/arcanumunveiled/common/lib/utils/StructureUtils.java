17c780e41210ea4d5df29eb493557f39f3db998f
Unicornbl00d <unicornbloods@protonmail.com>
2025-11-21 20:26:54 -0600

    Initial: Add the StructureUtils.java file, this handles the block replacements for structures. You probably would like this with that prior commit.

---
+++ src/main/java/xyz/uniblood/arcanumunveiled/common/lib/utils/StructureUtils.java
@@ -0,0 +1,47 @@
+package xyz.uniblood.arcanumunveiled.common.lib.utils;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.world.gen.structure.StructureComponent;
+import thaumcraft.common.config.ConfigBlocks;
+import xyz.uniblood.arcanumunveiled.compat.EtFuturum;
+
+import java.util.Random;
+
+public class StructureUtils {
+
+    public static class HilltopStonesVines extends StructureComponent.BlockSelector {
+        public HilltopStonesVines() {
+        }
+
+        /**
+         * picks Block Ids and Metadata
+         */
+        public void selectBlocks(Random rand, int x, int y, int z, boolean wall) {
+            if (rand.nextInt(3) != 0) {
+                this.field_151562_a = Blocks.air;
+            }
+        }
+    }
+
+    public static class HilltopStonesStone extends StructureComponent.BlockSelector {
+        public HilltopStonesStone() {
+        }
+
+        /**
+         * picks Block Ids and Metadata
+         */
+        public void selectBlocks(Random rand, int x, int y, int z, boolean wall) {
+            // For the vine
+            if (rand.nextBoolean()) {
+                if (EtFuturum.IS_LOADED && rand.nextInt(4) == 0) {
+                    this.field_151562_a = EtFuturum.CRYING_OBSIDIAN;
+                } else {
+                    this.field_151562_a = Blocks.obsidian;
+                }
+            } else {
+                this.field_151562_a = ConfigBlocks.blockCosmeticSolid;
+                this.selectedBlockMetaData = 1;
+            }
+        }
+    }
+}
