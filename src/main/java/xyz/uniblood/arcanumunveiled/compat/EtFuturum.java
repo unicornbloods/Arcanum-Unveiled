b30a95839cee878f5b2f2b875cc0723ed7e3f5f5
Unicornbl00d <unicornbloods@protonmail.com>
2025-11-21 20:23:12 -0600

    Initial: Cache file for mod compatibility with EtFuturum.

---
+++ src/main/java/xyz/uniblood/arcanumunveiled/compat/EtFuturum.java
@@ -0,0 +1,8 @@
+package xyz.uniblood.arcanumunveiled.compat;
+
+import net.minecraft.block.Block;
+
+public class EtFuturum {
+    public static boolean IS_LOADED;
+    public static Block CRYING_OBSIDIAN;
+}
