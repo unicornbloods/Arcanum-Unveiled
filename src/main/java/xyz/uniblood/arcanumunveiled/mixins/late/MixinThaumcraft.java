cbe6b7a09a207dc8c0105026720b514c94df2f8c
Unicornbl00d <unicornbloods@protonmail.com>
2025-12-03 21:04:38 -0600

    Initial: Why you loading that stuff Thaumcraft? You can stop doing that now, its ok, we are here.

--- src/main/java/xyz/uniblood/arcanumunveiled/mixins/late/MixinThaumcraft.java
+++ src/main/java/xyz/uniblood/arcanumunveiled/mixins/late/MixinThaumcraft.java
@@ -1,11 +1,23 @@
 package xyz.uniblood.arcanumunveiled.mixins.late;
 
+import cpw.mods.fml.common.IWorldGenerator;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import thaumcraft.common.Thaumcraft;
+import thaumcraft.common.config.Config;
+import thaumcraft.common.config.ConfigAspects;
+import thaumcraft.common.config.ConfigEntities;
+import thaumcraft.common.config.ConfigItems;
+import thaumcraft.common.config.ConfigRecipes;
+import thaumcraft.common.config.ConfigResearch;
+import thaumcraft.common.lib.world.ThaumcraftWorldGenerator;
 
 @Mixin(value = Thaumcraft.class, remap = false)
 public class MixinThaumcraft {
@@ -18,5 +30,33 @@ public class MixinThaumcraft {
     )
     private void cancelDimensionRegister(FMLInitializationEvent evt, CallbackInfo ci) {
         ci.cancel();
+    }
+
+    @Redirect(method = "preInit", at = @At(value = "INVOKE", target = "Lcpw/mods/fml/common/registry/GameRegistry;registerWorldGenerator(Lcpw/mods/fml/common/IWorldGenerator;I)V"))
+    public void removeWorldGenRegister(IWorldGenerator generator, int modGenerationWeight) {
+        return;
+    }
+
+    @Redirect(method = "preInit", at = @At(value = "INVOKE", target = "Lthaumcraft/common/lib/world/ThaumcraftWorldGenerator;initialize()V"))
+    public void removeWorldGenInit(ThaumcraftWorldGenerator instance) {
+        return;
+    }
+
+    /**
+     * @author Uniblood
+     * @reason test
+     */
+    @Mod.EventHandler
+    @Overwrite()
+    public void postInit(FMLPostInitializationEvent evt) {
+        Config.initPotions();
+        ConfigEntities.initEntitySpawns();
+        Config.initModCompatibility();
+        ConfigItems.postInit();
+        ConfigRecipes.init();
+        ConfigAspects.init();
+        ConfigResearch.init();
+        Config.initLoot();
+        Config.initMisc();
     }
 }
